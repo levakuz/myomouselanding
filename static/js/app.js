b35515f20e11b1491116fb0ae9f1839c3fe89105
levakuz <levakuz007@gmail.com>
2022-06-06 16:47:29 +0300

    fixed: freeze and positioning

--- static/js/app.js
+++ static/js/app.js
@@ -2,176 +2,116 @@
 // import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
 import * as THREE from "./three.module.js"
 import {OrbitControls} from "./three/examples/jsm/controls/OrbitControls.js";
-// import {GLTFLoader} from "./three/examples/jsm/loaders/GLTFLoader.js";
+import {GLTFLoader} from "./three/examples/jsm/loaders/GLTFLoader.js";
 
 var APP = {
 
 	Player: function () {
 
-		var renderer = new THREE.WebGLRenderer( { antialias: true } );
+		var renderer = new THREE.WebGLRenderer( { antialias: true, 		"shadows": true,
+		"shadowType": 1,
+		"vr": false,
+		"physicallyCorrectLights": true,
+		"toneMapping": 2,
+		"toneMappingExposure": 1} );
 		renderer.setPixelRatio( window.devicePixelRatio ); // TODO: Use player.setPixelRatio()
 		renderer.outputEncoding = THREE.sRGBEncoding;
-
-		var loader = new THREE.ObjectLoader();
-		var camera, scene;
-
+		const scene = new THREE.Scene();
+		let camera
 		var vrButton = VRButton.createButton( renderer ); // eslint-disable-line no-undef
 
 		var events = {};
-
+		let controls
 		var dom = document.createElement( 'div' );
 		dom.appendChild( renderer.domElement );
 
 		this.dom = dom;
 
-		this.width = 500;
-		this.height = 500;
-		// const loader = new GLTFLoader();
-// loader.load(
-// 	// resource URL
-// 	'static/scene.glb',
-// 	// called when the resource is loaded
-// 	function ( gltf ) {
-// 		console.log(gltf.scene)
-// 		scene.add( gltf.scene );
-//
-// 		gltf.animations; // Array<THREE.AnimationClip>
-// 		gltf.scene; // THREE.Group
-// 		gltf.scenes; // Array<THREE.Group>
-// 		gltf.cameras; // Array<THREE.Camera>
-// 		gltf.asset; // Object
-// 		// this.setCamera(gltf.cameras)
-//
-// 	},
-// 	// called while loading is progressing
-// 	function ( xhr ) {
-//
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-//
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-//
-// 		console.log( 'An error happened' );
-//
-// 	}
-// );
-		this.load = function ( json ) {
 
-			var project = json.project;
+		const loader = new GLTFLoader();
+		loader.load(
+			// resource URL
+			'static/scene.glb',
+			// called when the resource is loaded
+			function ( gltf ) {
+				console.log(gltf.scene)
+				scene.add( gltf.scene );
 
-			if ( project.vr !== undefined ) renderer.xr.enabled = project.vr;
-			if ( project.shadows !== undefined ) renderer.shadowMap.enabled = project.shadows;
-			if ( project.shadowType !== undefined ) renderer.shadowMap.type = project.shadowType;
-			if ( project.toneMapping !== undefined ) renderer.toneMapping = project.toneMapping;
-			if ( project.toneMappingExposure !== undefined ) renderer.toneMappingExposure = project.toneMappingExposure;
-			if ( project.physicallyCorrectLights !== undefined ) renderer.physicallyCorrectLights = project.physicallyCorrectLights;
-
-			this.setScene( loader.parse( json.scene ) );
-			this.setCamera( loader.parse( json.camera ) );
-			const controls = new OrbitControls( camera, renderer.domElement );
-			controls.update();
-			console.log(scene)
-
-
-
-
-			function animate() {
-
-				requestAnimationFrame( animate );
-
-				// required if controls.enableDamping or controls.autoRotate are set to true
-				controls.update();
-
-				renderer.render( scene, camera );
-
-			}
-
-
-			events = {
-				init: [],
-				start: [],
-				stop: [],
-				keydown: [],
-				keyup: [],
-				pointerdown: [],
-				pointerup: [],
-				pointermove: [],
-				update: []
-			};
-
-			var scriptWrapParams = 'player,renderer,scene,camera';
-			var scriptWrapResultObj = {};
-
-			for ( var eventKey in events ) {
-
-				scriptWrapParams += ',' + eventKey;
-				scriptWrapResultObj[ eventKey ] = eventKey;
-
-			}
-
-			var scriptWrapResult = JSON.stringify( scriptWrapResultObj ).replace( /\"/g, '' );
-
-			for ( var uuid in json.scripts ) {
-
-				var object = scene.getObjectByProperty( 'uuid', uuid, true );
-				if ( object === undefined ) {
-
-					console.warn( 'APP.Player: Script without object.', uuid );
-					continue;
-
+				gltf.animations; // Array<THREE.AnimationClip>
+				gltf.scene; // THREE.Group
+				gltf.scenes; // Array<THREE.Group>
+				gltf.cameras; // Array<THREE.Camera>
+				gltf.asset; // Object
+				// this.setCamera(gltf.cameras)
+				var canvas_wrapper = document.getElementById('canvas_wrapper')
+				let loading_status_wrapper = document.getElementById('loading_status_wrapper')
+                canvas_wrapper.removeChild(loading_status_wrapper);
+                canvas_wrapper.appendChild( renderer.domElement );
+				let ambient = scene.getObjectByProperty('name', "AmbientLight")
+				ambient.removeFromParent(scene)
+				let spotlight = scene.getObjectByProperty('name', "SpotLight")
+				spotlight.removeFromParent(scene)
+				let spotlight1 = scene.getObjectByProperty('name', "SpotLight_2")
+				spotlight1.removeFromParent(scene)
+				let spotlight2 = scene.getObjectByProperty('name', "SpotLight_4")
+				spotlight2.removeFromParent(scene)
+				const spotLight = new THREE.SpotLight( 0xffffff )
+				spotLight.position.set(5,10,-14.7)
+				scene.add(spotLight)
+				const spotLight2 = new THREE.SpotLight( 0xffffff )
+				spotLight2.position.set(9,6,13.2)
+				scene.add(spotLight2)
+				const spotLight3 = new THREE.SpotLight( 0xffffff )
+				spotLight3.position.set(-3.85,3,13.6)
+				scene.add(spotLight3)
+				scene.add(gltf.cameras[0])
+				if (window.screen.width >= 1700){
+					// player.setSize( 840, 640);
+					renderer.setSize(640, 640)
 				}
-
-				var scripts = json.scripts[ uuid ];
-
-				for ( var i = 0; i < scripts.length; i ++ ) {
-
-					var script = scripts[ i ];
-
-					var functions = ( new Function( scriptWrapParams, script.source + '\nreturn ' + scriptWrapResult + ';' ).bind( object ) )( this, renderer, scene, camera );
-
-					for ( var name in functions ) {
-
-						if ( functions[ name ] === undefined ) continue;
-
-						if ( events[ name ] === undefined ) {
-
-							console.warn( 'APP.Player: Event type not supported (', name, ')' );
-							continue;
-
-						}
-
-						events[ name ].push( functions[ name ].bind( object ) );
-
-					}
-
+				else
+				{
+					// player.setSize( 300, 200);
+					renderer.setSize(300, 300)
 				}
+				camera = gltf.cameras[0]
+				camera.fov = 50
+				camera.position.x = 6
+				camera.position.y = 3.633
+				camera.position.x = 9.666
+				camera.far = 100
+				camera.aspect = 1
+			   	camera.fov = 50
+			  	camera.zoom = 1
+			  	camera.near = 0.01
+				camera.far = 1000
+				camera.focus = 10
+				// camera.aspect =  1.5256410256410255
+				camera.filmGauge = 35
+				camera.filmOffset = 0
+				camera.updateProjectionMatrix();
+				console.log(camera)
+				controls = new OrbitControls( camera, renderer.domElement );
+				controls.update()
+				animate()
+
+			},
+			// called while loading is progressing
+			function ( xhr ) {
+
+				console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+				let loading_status = document.getElementById('loading_status')
+                const node = document.createTextNode((xhr.loaded / xhr.total * 100).toFixed(2) + '% loaded')
+                loading_status.value= (xhr.loaded / xhr.total * 100)
+			},
+			// called when loading has errors
+			function ( error ) {
+
+				console.log( 'An error happened' );
 
 			}
+		);
 
-			dispatch( events.init, arguments );
-
-		};
-
-		this.setCamera = function ( value ) {
-
-			camera = value;
-			camera.aspect = this.width / this.height;
-			camera.updateProjectionMatrix();
-
-		};
-
-		this.setScene = function ( value ) {
-
-			scene = value;
-
-		};
-
-		this.setPixelRatio = function ( pixelRatio ) {
-
-			renderer.setPixelRatio( pixelRatio );
-
-		};
 
 		this.setSize = function ( width, height ) {
 
@@ -180,8 +120,8 @@ var APP = {
 
 			if ( camera ) {
 
-				camera.aspect = this.width / this.height;
-				camera.updateProjectionMatrix();
+				// camera.aspect = width / height
+				// camera.updateProjectionMatrix();
 
 			}
 
@@ -200,35 +140,17 @@ var APP = {
 		}
 
 		var time, startTime, prevTime;
-
 		function animate() {
 
-			time = performance.now();
+			requestAnimationFrame( animate );
 
-			try {
-
-				dispatch( events.update, { time: time - startTime, delta: time - prevTime } );
-
-			} catch ( e ) {
-
-				console.error( ( e.message || e ), ( e.stack || '' ) );
-
-			}
+			// required if controls.enableDamping or controls.autoRotate are set to true
+			controls.update();
 
 			renderer.render( scene, camera );
 
-			prevTime = time;
-
 		}
 
-
-		// function chagelinesColor(color){
-		// 		let a = scene.getObjectByProperty('name', "2liner")
-		// 		let b = scene.getObjectByProperty('name', "2linel")
-		// 		console.log(a.material.color)
-		// 		a.material.color.setHex(color)
-		// 		b.material.color.setHex(color)
-		// 	}
 
 		this.play = function () {
 
@@ -312,7 +234,7 @@ var APP = {
 			dispatch( events.pointermove, event );
 
 		}
-				var white_line_button = document.getElementById('white-line-button')
+		var white_line_button = document.getElementById('white-line-button')
 		white_line_button.addEventListener('click', function (){chagelinesColor('white')})
 		var grey_line_button = document.getElementById('grey-line-button')
 		grey_line_button.addEventListener('click', function (){chagelinesColor('grey')})
@@ -356,6 +278,27 @@ var APP = {
 			orange: '0xff5722',
 			yellow: '0xfbc02d'
 		}
+		const line_buttons = {
+			white: white_line_button,
+			grey: grey_line_button,
+			black: black_line_button,
+			blue: blue_line_button,
+			green: green_line_button,
+			red: red_line_button,
+			orange: orange_line_button,
+			yellow: yellow_line_button
+		}
+
+		const band_buttons = {
+			white: white_band_button,
+			grey: grey_band_button,
+			black: black_band_button,
+			blue: blue_band_button,
+			green: green_band_button,
+			red: red_band_button,
+			orange: orange_band_button,
+			yellow: yellow_band_button
+		}
 		// var lines_color_input = document.getElementById("linecolorpicker")
 		// console.log(lines_color_input)
 		// lines_color_input.addEventListener('change', function (color){
@@ -374,10 +317,24 @@ var APP = {
 			let b = scene.getObjectByProperty('name', "2linel")
 			a.material.color.setHex(colors[color])
 			b.material.color.setHex(colors[color])
+			for(let i in line_buttons){
+				line_buttons[i].style.borderWidth = 0
+			}
+			let active_btn = line_buttons[color]
+			active_btn.style.borderColor = 'white'
+			active_btn.style.borderWidth = '2px'
+			active_btn.style.padding = '2px'
 		}
 		function chagebracerColor(color){
 			let a = scene.getObjectByProperty('name', "bracer")
 			a.material.color.setHex(colors[color])
+			for(let i in band_buttons){
+				band_buttons[i].style.borderWidth = 0
+			}
+			let active_btn = band_buttons[color]
+			active_btn.style.borderColor = 'white'
+			active_btn.style.borderWidth = '2px'
+			active_btn.style.padding = '2px'
 		}
 
 	}
